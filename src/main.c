1bb8d44bb9bedac6484084e2e8755cc7110198bb
outsiderice <arantxamagnelli@gmail.com>
2024-08-19 20:48:52 +0200

    removing executable

--- src/main.c
+++ src/main.c
@@ -6,13 +6,15 @@
 /*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 20:16:43 by amagnell          #+#    #+#             */
-/*   Updated: 2024/08/19 20:21:04 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/08/19 20:44:55 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
+#include <stdio.h>
 
-int	main()
+int	main(int argc, char **argv)
 {
+	printf("This is philosophers\n");
 	return (0);
 }
