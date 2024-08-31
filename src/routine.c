5e315841d4778d9fe38eaddcaa2d8b16b41a4910
outsiderice <arantxamagnelli@gmail.com>
2024-08-31 17:10:45 +0200

    removing gettimeofday return portections for flag

--- src/routine.c
+++ src/routine.c
@@ -6,43 +6,35 @@
 /*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 09:40:33 by amagnell          #+#    #+#             */
-/*   Updated: 2024/08/31 17:05:36 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/08/31 17:10:19 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 #include <unistd.h>
 
-int	sleeping(t_philo *philo)
+void	sleeping(t_philo *philo)
 {
-	if (print_msg(philo, "is sleeping") == 1)
-		return (EXIT_FAILURE);
-	if (time_passes(philo, philo->t->to_sleep) == 1)
-		return (EXIT_FAILURE);
-	return (EXIT_SUCCESS);
+	print_msg(philo, "is sleeping")
+	time_passes(philo, philo->t->to_sleep)
 }
 
 int	eating(t_philo *philo)
 {
 	int	wait_start;
 
-	if (print_msg(philo, "is thinking") == 1)
-		return (EXIT_FAILURE);
+	print_msg(philo, "is thinking")
 	wait_start = ft_time(philo->t);
 	pthread_mutex_lock(philo->r_fork);
 	philo->timer = philo->timer + (ft_time(philo->t) - wait_start);
-	if (print_msg(philo, "has taken a fork") == 1)
-		return (EXIT_FAILURE);
+	print_msg(philo, "has taken a fork")
 	wait_start = ft_time(philo->t);
 	pthread_mutex_lock(philo->l_fork);
 	philo->timer = philo->timer + (ft_time(philo->t) - wait_start);
-	if (print_msg(philo, "has taken a fork") == 1)
-		return (EXIT_FAILURE);
-	if (print_msg(philo, "is eating") == 1)
-		return (EXIT_FAILURE);
+	print_msg(philo, "has taken a fork")
+	print_msg(philo, "is eating")
 	philo->timer = 0;
-	if (time_passes(philo, philo->t->to_eat) == 1)
-		return (EXIT_FAILURE);
+	time_passes(philo, philo->t->to_eat)
 	pthread_mutex_unlock(philo->r_fork);
 	pthread_mutex_unlock(philo->l_fork);
 	philo->meals_left--;
@@ -69,15 +61,12 @@ int	philo_loop(t_philo *philo)
 	return (EXIT_SUCCESS);
 }
 
-int	die_alone(t_philo *philo)
+void	die_alone(t_philo *philo)
 {
 	pthread_mutex_lock(philo->r_fork);
 	pthread_mutex_unlock(philo->r_fork);
-	if (print_msg(philo, "has taken a fork") == 1)
-		return (EXIT_FAILURE);
-	if (time_passes(philo, philo->t->to_die) == 1)
-		return (EXIT_FAILURE);
-	return (EXIT_SUCCESS);
+	print_msg(philo, "has taken a fork")
+	time_passes(philo, philo->t->to_die)
 }
 
 void	*philo_start(t_philo *philo)
