e35048da2a02b5980b59b2f55faac42bf27cfd32
outsiderice <arantxamagnelli@gmail.com>
2024-09-04 09:15:47 +0200

    details

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 09:40:33 by amagnell          #+#    #+#             */
-/*   Updated: 2024/09/03 15:41:25 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/09/04 09:09:00 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@ void	sleeping(t_philo *philo)
 {
 	print_msg(philo, "is sleeping");
 	time_passes(philo, philo->t->to_sleep);
+	time_passes(philo, 1);
 }
 
-void	thinking(t_philo *philo)
+void	thinking(t_philo *philo, int even)
 {
 	print_msg(philo, "is thinking");
-	if (philo->name == philo->t->n_philos && philo->t->n_philos % 2 == 0)
+	if (philo->name == philo->t->n_philos && even == 1)
 	{
 		pthread_mutex_lock(philo->l_fork);
 		pthread_mutex_lock(philo->r_fork);
@@ -36,15 +37,23 @@ void	thinking(t_philo *philo)
 	print_msg(philo, "has taken a fork");
 }
 
-void	eating(t_philo *philo)
+void	eating(t_philo *philo, int even)
 {
 	print_msg(philo, "is eating");
 	pthread_mutex_lock(&philo->d_lock);
 	philo->time_of_death = ft_time(philo->t) + philo->t->to_die;
 	pthread_mutex_unlock(&philo->d_lock);
 	time_passes(philo, philo->t->to_eat);
-	pthread_mutex_unlock(philo->r_fork);
-	pthread_mutex_unlock(philo->l_fork);
+	if (philo->name == philo->t->n_philos && even == 1)
+	{
+		pthread_mutex_unlock(philo->r_fork);
+		pthread_mutex_unlock(philo->l_fork);
+	}
+	else
+	{
+		pthread_mutex_unlock(philo->l_fork);
+		pthread_mutex_unlock(philo->r_fork);
+	}
 	if (philo->t->meals > 0 && philo->meals_left > 0)
 	{
 		philo->meals_left--;
@@ -60,12 +69,16 @@ void	eating(t_philo *philo)
 void	philo_loop(t_philo *philo)
 {
 	int	stop;
+	int	even;
 
 	stop = 0;
+	even = 0;
+	if (philo->t->n_philos % 2 == 0)
+		even = 1;
 	while (stop != 1)
 	{
-		thinking(philo);
-		eating(philo);
+		thinking(philo, even);
+		eating(philo, even);
 		sleeping(philo);
 		pthread_mutex_lock(&philo->t->end_lock);
 		stop = philo->t->end;
